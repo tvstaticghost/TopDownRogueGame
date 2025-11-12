2b5a4da6dcc53745c359e97450e7767426746a81
Ray Arnold <rarno93@wgu.edu>
2025-11-11 22:33:01 -0500

    improved procedural map generation logic and added a left map fragment

--- Scripts/test_generated_level.gd
+++ Scripts/test_generated_level.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 const LEFT_SECTION_1 = preload("uid://dwl1n83o3olms")
+const LEFT_SECTION_2 = preload("uid://7mr8ae62p42i")
 const RIGHT_SECTION_1 = preload("uid://c5xjrbc6kaho6")
 const PLAYER = preload("uid://6n24mvh65b57")
 
 #Add fragments to each list when they are created and preloaded
-var LEFT_SECTIONS = [LEFT_SECTION_1]
+var LEFT_SECTIONS = [LEFT_SECTION_1, LEFT_SECTION_2]
 var RIGHT_SECTIONS = [RIGHT_SECTION_1]
 
 var SPAWN_POSITIONS = []
@@ -13,19 +14,23 @@ var SPAWN_POSITIONS = []
 #TODO THis is insanely broken
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var left_test = LEFT_SECTIONS[0].instantiate()
+	var random_left_frag = LEFT_SECTIONS[randi() % LEFT_SECTIONS.size()]
+	var left_test = random_left_frag.instantiate()
 	add_child(left_test)
 	
 	var right_test = RIGHT_SECTIONS[0].instantiate()
 	add_child(right_test)
 	
-	var right_conn_point = right_test.get_child(0)
-	var left_conn_point = left_test.get_child(0)
-	
 	SPAWN_POSITIONS.append(right_test.get_child(1).global_position)
 	SPAWN_POSITIONS.append(left_test.get_child(1).global_position)
 	
-	var offset = right_conn_point.position  # local to right_test
-	right_test.global_position = left_conn_point.global_position - offset
+	var left_marker_pos = left_test.get_child(0).global_position
+	var right_marker_pos = right_test.get_child(0).global_position
 	
-	print(SPAWN_POSITIONS)
+	var difference = left_marker_pos - right_marker_pos
+	right_test.global_position = difference
+	
+	var spawn_pos_index = randi_range(0, len(SPAWN_POSITIONS) - 1)
+	var player_object = PLAYER.instantiate()
+	add_child(player_object)
+	player_object.global_position = SPAWN_POSITIONS[spawn_pos_index]
